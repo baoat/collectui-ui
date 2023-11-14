a04ff27e34ebe7690597dec538e1a50e8164ccb8
baoat <baoat191201@gmail.com>
2023-11-14 16:39:59 +0700

    update file GlobalStyle.scss

--- src/layouts/components/MobileHeader/index.js
+++ src/layouts/components/MobileHeader/index.js
@@ -2,22 +2,47 @@ import style from "./MobileHeader.module.scss";
 import classNames from "classnames/bind";
 
 import Logo from "../../../components/icons/Logo/Logo";
+import { useState } from "react";
 
 const cx = classNames.bind(style);
 
 function MobileHeader() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <>
       <a className={cx("mobile-logo")} href="#">
         <Logo className={cx("mobile-logo__logo")} />
       </a>
       <div className={cx("mobile-header")}></div>
-      <label id="nav-icon" className={cx("nav-icon")}>
+      <div
+        onClick={() => {
+          setShowMenu(!showMenu);
+        }}
+        id="nav-icon"
+        className={showMenu ? cx("nav-icon-x") : cx("nav-icon")}
+      >
         <span></span>
         <span></span>
         <span></span>
         <span></span>
-      </label>
+      </div>
+      <div className={cx("full-menu", "animated", "fadeIn")}>
+        <ul>
+          <li className={cx("animated", "fadeIn")}>
+            <a href="#">Design</a>
+          </li>
+          <li className={cx("animated", "fadeIn")}>
+            <a href="#">Designers</a>
+          </li>
+          <li className={cx("animated", "fadeIn")}>
+            <a href="#">Challenges</a>
+          </li>
+          <li className={cx("animated", "fadeIn")}>
+            <a href="#">About</a>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
